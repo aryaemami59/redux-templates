9284224da35360feae3d073687804d5d0420577f
Arya Emami <aryaemami59@yahoo.com>
2024-03-16 09:21:34 -0500

    Refactor mockTemplates.mjs script

--- scripts/mockTemplates.mjs
+++ scripts/mockTemplates.mjs
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { exec as _exec } from 'node:child_process'
-import fs from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { promisify } from 'node:util'
@@ -10,16 +9,6 @@ const exec = promisify(_exec)
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
-const getCommitHash = async () => {
-  try {
-    const { stdout } = await exec('git rev-parse --short HEAD')
-    return stdout.trim()
-  } catch (error) {
-    console.error('Failed to get commit hash:', error)
-    throw error
-  }
-}
 
 const listYarnWorkspaces = async () => {
   try {
@@ -58,28 +47,15 @@ const listYarnWorkspaces = async () => {
 
 const workspaces = await listYarnWorkspaces()
 
-const commitHash = await getCommitHash()
-
 async function constructGitHubUrl() {
   try {
-    // Get the URL of the current remote repository
     const remoteUrl = (await exec('git remote get-url origin')).stdout.trim()
-    // Transform SSH URL to HTTPS URL if necessary
-    // remoteUrl = remoteUrl.replace(/^git@([^:]+):/, 'https://$1/').replace('.git', '');
 
-    // Get the name of the current branch
     const currentBranch = (
       await exec('git branch --show-current')
     ).stdout.trim()
 
-    // Get the latest commit hash
-    const commitHash = (await exec('git rev-parse HEAD')).stdout.trim()
-
-    // Construct the URL
-    // Assuming the 'packages/vite-template-redux#convert-to-monorepo' part is static
-    // const url = `${remoteUrl}/tree/${currentBranch}/packages/vite-template-redux#convert-to-monorepo#${commitHash}`;
-
-    // console.log(`URL: ${url}`);
+    const commitHash = (await exec('git rev-parse --short HEAD')).stdout.trim()
 
     return {
       remoteUrl,
@@ -93,82 +69,16 @@ async function constructGitHubUrl() {
 
 const gitHubUrl = await constructGitHubUrl()
 
-const outputFolderNames = new Map([
-  ['cra-template-redux', `cra-js-app`],
-  ['cra-template-redux-typescript', `cra-ts-app`],
-  ['expo-template-redux-typescript', `expo-ts-app`],
-  ['react-native-template-redux-typescript', `rn-ts-app`],
-  ['vite-template-redux', `vite-ts-app`],
-])
-
 const allTemplates = {
   'cra-template-redux': `npx create-react-app example --template file:${workspaces?.get('cra-template-redux')}`,
   'cra-template-redux-typescript': `npx create-react-app@latest example --template file:${workspaces?.get('cra-template-redux-typescript')}`,
   'expo-template-redux-typescript': `npx create-expo@latest example --template file:${workspaces?.get('expo-template-redux-typescript')}`,
   'react-native-template-redux-typescript': `npx react-native@latest init app --template file:${workspaces?.get('react-native-template-redux-typescript')} --pm=npm --directory example`,
-  'vite-template-redux': `npx tiged --mode=git ${gitHubUrl?.remoteUrl}/packages/vite-template-redux#${gitHubUrl?.currentBranch} example -v && cd example && npm install`,
+  'vite-template-redux': `npx tiged --mode=git ${gitHubUrl?.remoteUrl}/packages/vite-template-redux#${gitHubUrl?.currentBranch}#${gitHubUrl?.commitHash} example -v && cd example && npm install`,
 }
 
 const mockTemplate = async (template) => {
   await exec(allTemplates[template])
 }
 
-const matrixList = Object.values(allTemplates).map((pkg) => ({
-  package: pkg,
-}))
-
-const includeStatement = { include: matrixList }
-// console.log(`::set-output name=matrix::${JSON.stringify(includeStatement)}`)
-
-const removeMockedTemplateDirectory = async (outputFolderName) => {
-  await fs.rm(path.resolve(__dirname, '..', outputFolderName), {
-    recursive: true,
-    force: true,
-  })
-}
-
-const mockTemplates = async () => {
-  Object.entries(allTemplates).forEach(async ([templateName, command]) => {
-    const outputFolderName = outputFolderNames.get(templateName)
-    console.log(`Mocking ${templateName}...`)
-    try {
-      const { stdout } = await exec(command)
-      console.log(stdout)
-    } catch (err) {
-      console.error(err)
-      console.log(`Failed to create ${templateName}! Exiting...`)
-      await removeMockedTemplateDirectory(outputFolderName)
-      process.exit(1)
-    }
-
-    try {
-      console.log('Running tests...')
-      // const { stdout } = await exec(
-      //   `cd ${outputFolderName} && set CI=true && npm run test`,
-      // )
-      // console.log(stdout)
-    } catch (err) {
-      console.error(err)
-      console.log(`Tests failed for ${templateName}! Exiting...`)
-      await removeMockedTemplateDirectory(outputFolderName)
-      process.exit(1)
-    }
-
-    try {
-      console.log('Building...')
-      // const { stdout } = await exec(`cd ${outputFolderName} && npm run build`)
-      // console.log(stdout)
-    } catch (err) {
-      console.error(err)
-      console.log(`Build failed for ${templateName}! Exiting...`)
-      process.exit(1)
-    } finally {
-      console.log('Cleaning up...')
-      await removeMockedTemplateDirectory(outputFolderName)
-    }
-  })
-}
-
-// await mockTemplates()
-
-mockTemplate(process.argv[2])
+await mockTemplate(process.argv[2])
